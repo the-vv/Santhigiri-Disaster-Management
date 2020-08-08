705f9a628d98fb23e13bdf486ad38e0bc5f3075a
vv-2001 <vishnuvinod2772001@gmail.com>
2020-08-09 00:37:45 +0530

    added firebase database scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCRAE1JLUe2Nt8sWFo5_RhVuId_2vMgrZM",
+  authDomain: "registration-91b64.firebaseapp.com",
+  databaseURL: "https://registration-91b64.firebaseio.com",
+  projectId: "registration-91b64",
+  storageBucket: "registration-91b64.appspot.com",
+  messagingSenderId: "444371260564",
+  appId: "1:444371260564:web:0bbccb6e41cf5fe775d081"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
