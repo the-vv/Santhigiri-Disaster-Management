9f7fcdd285e4b935f6f66804184e26efc67e7592
vv-2001 <vishnuvinod2772001@gmail.com>
2020-08-15 00:34:45 +0530

    get weather objects

--- src/app/home-content/home-content.component.ts
+++ src/app/home-content/home-content.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { LocationService } from '../services/location.service';
 import { WeatherService } from '../services/weather.service';
 
+export class Weather{
+  Date: string;
+  EpochDate: number;
+  Temperature: Array<any>;
+  Day:Array<any>;
+  Night:Array<any>;
+}
+
+
 @Component({
   selector: 'app-home-content',
   templateUrl: './home-content.component.html',
@@ -12,7 +21,9 @@ export class HomeContentComponent implements OnInit {
   lat: number;
   lot: number;
   locationKey: number;
-  weather: Array<any>;
+  weather: Weather;
+  DailyForecasts:Array<any> = [];
+  Dailyweather: Array<any>;
 
   constructor(
     private Location: LocationService,
@@ -27,6 +38,7 @@ export class HomeContentComponent implements OnInit {
       console.log(`Positon: ${this.lot} ${this.lat}`);
       this.getWeatherkey();
     })
+
   }
   
   getWeatherkey(){
@@ -42,9 +54,66 @@ export class HomeContentComponent implements OnInit {
   getWeather(){
     this.wKey.getWeather(this.locationKey)
     .subscribe(response=>{
-      this.weather = response
+      this.weather = response.DailyForecasts[0]
       console.log(this.weather)
+      // this.getDaily();
     })
+  }
+
+  // getDaily(){
+  //   for(let daily of this.data.DailyForecasts){
+  //     this.DailyForecasts.push(daily);
+  //   }
+  //   this.Dailyweather = this.DailyForecasts[0];
+  // }
+
+  data = {
+    "Headline": {
+      "EffectiveDate": "2020-08-14T19:00:00+01:00",
+      "EffectiveEpochDate": 1597428000,
+      "Severity": 4,
+      "Text": "Air quality will be unhealthy for sensitive groups Friday evening through Monday afternoon",
+      "Category": "air quality",
+      "EndDate": "2020-08-17T19:00:00+01:00",
+      "EndEpochDate": 1597687200,
+      "MobileLink": "http://m.accuweather.com/en/ng/miango/255093/extended-weather-forecast/255093?lang=en-us",
+      "Link": "http://www.accuweather.com/en/ng/miango/255093/daily-weather-forecast/255093?lang=en-us"
+    },
+    "DailyForecasts": [
+      {
+        "Date": "2020-08-14T07:00:00+01:00",
+        "EpochDate": 1597384800,
+        "Temperature": {
+          "Minimum": {
+            "Value": 65,
+            "Unit": "F",
+            "UnitType": 18
+          },
+          "Maximum": {
+            "Value": 76,
+            "Unit": "F",
+            "UnitType": 18
+          }
+        },
+        "Day": {
+          "Icon": 15,
+          "IconPhrase": "Thunderstorms",
+          "HasPrecipitation": true,
+          "PrecipitationType": "Rain",
+          "PrecipitationIntensity": "Light"
+        },
+        "Night": {
+          "Icon": 38,
+          "IconPhrase": "Mostly cloudy",
+          "HasPrecipitation": false
+        },
+        "Sources": [
+          "AccuWeather"
+        ],
+        "MobileLink": "http://m.accuweather.com/en/ng/miango/255093/daily-weather-forecast/255093?day=1&lang=en-us",
+        "Link": "http://www.accuweather.com/en/ng/miango/255093/daily-weather-forecast/255093?day=1&lang=en-us"
+      }
+    ]
   }
 
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { LocationService } from '../services/location.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-
-  weatherkeyURL = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=Z12rLGHLn6PxyxMLhvfiWQ8kAmcihdCr&q=10.8505159%2C%2076.2710833'
 
   constructor(
     private http: HttpClient) {
